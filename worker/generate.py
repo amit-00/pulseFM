599ce7c4a84e172219f70cb6ff7adf9b816530eb
Amit Verma <amit.v@hotmail.com>
2026-01-17 18:48:35 -0500

    add ability to read/write to firestore db to read and update song requests

--- worker/generate.py
+++ worker/generate.py
@@ -1,12 +1,33 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+import uuid
+from datetime import datetime
+from typing import Annotated
 
+from fastapi import FastAPI, HTTPException, Response
+from pydantic import BaseModel, AfterValidator
+from google.cloud import firestore
+
+
+db = firestore.AsyncClient()
 app = FastAPI()
 
+
 class GenerateRequest(BaseModel):
-    request_id: str
+    request_id: Annotated[
+        str, 
+        AfterValidator(lambda x: str(uuid.uuid4()))
+    ]
 
 
 @app.post("/generate")
-def generate(payload: GenerateRequest):
-    print(f"Generating for request {payload.request_id}")
+async def generate(payload: GenerateRequest):
+    request_ref = db.collection("requests").document(payload.request_id)
+    request = await request_ref.get()
+
+    if not request.exists:
+        raise HTTPException(status_code=404, detail="Request not found")
+
+    request_ref.set({
+        "status": "generating"
+    })
+
+    return Response(status_code=204)
