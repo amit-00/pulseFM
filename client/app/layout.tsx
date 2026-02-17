0081a442c7bea3ab26278312783b8b3843a97acf
Amit Verma <amit.v@hotmail.com>
2026-02-16 23:52:42 -0500

    apply color to html

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -23,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-stone-950">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-stone-950`}
       >
         {children}
       </body>
